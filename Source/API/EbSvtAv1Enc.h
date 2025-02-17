vghuy17ck1/svt-av1-psyex

commit 988e930c1083ce518ead1d364e3a486e9209bf73

Public API clean-up and update

Source/API/EbSvtAv1Enc.h
@@ -20,7 +20,9 @@ extern "C" {
 #include "EbSvtAv1.h"
 #include <stdlib.h>
 #include <stdio.h>
-
+#if FIX_BOOL
+#include <stdbool.h>
+#endif
 /**
  * @brief SVT-AV1 encoder ABI version
  *
@@ -330,14 +332,14 @@ typedef struct EbSvtAv1EncConfiguration {
      * Default is YUV420.
      */
     EbColorFormat encoder_color_format;
-
+#if !FIX_HIGH_DYNAMIC_RANGE_INPUT
     /**
      * @brief Currently unused.
      *
      * Default is 0.
      */
     uint8_t high_dynamic_range_input;
-
+#endif
     /**
      * @brief Bitstream profile to use.
      * 0: main, 1: high, 2: professional.
@@ -354,7 +356,6 @@ typedef struct EbSvtAv1EncConfiguration {
      *
      * Default is 0. */
     uint32_t tier;
-
     /**
      * @brief Bitstream level.
      * 0: autodetect from bitstream, 20: level 2.0, 63: level 6.3, only levels 2.0-6.3 are properly defined.
@@ -366,7 +367,7 @@ typedef struct EbSvtAv1EncConfiguration {
      * Default is 0.
      */
     uint32_t level;
-
+#if !FIX_COLOR_DESCRIPTION_PRESENT_FLAG
     /* Color description present flag
     *
     * It is not necessary to set this parameter manually.
@@ -375,6 +376,7 @@ typedef struct EbSvtAv1EncConfiguration {
     *
     Default is false. */
     Bool color_description_present_flag;
+#endif
     /* Color primaries
     * values are from EbColorPrimaries
     Default is 2 (CP_UNSPECIFIED). */
@@ -416,8 +418,11 @@ typedef struct EbSvtAv1EncConfiguration {
      *
      * Refer to the SvtAv1RcMode enum for valid values
      * Default is 0. */
+#if FIX_RATE_CONTROL_MODE
+    uint8_t rate_control_mode;
+#else
     uint32_t rate_control_mode;
-
+#endif
     // Rate control tuning
 
     // Quantization
@@ -430,8 +435,11 @@ typedef struct EbSvtAv1EncConfiguration {
     /* force qp values for every picture that are passed in the header pointer
     *
     * Default is 0.*/
+#if FIX_BOOL
+    bool use_qp_file;
+#else
     Bool use_qp_file;
-
+#endif
     /* Target bitrate in bits/second, only applicable when rate control mode is
      * set to 1 (VBR) or 2 (CBR).
      *
@@ -442,12 +450,12 @@ typedef struct EbSvtAv1EncConfiguration {
      *
      * Default is 0. */
     uint32_t max_bit_rate;
-
+#if !FIX_VBV_BUFSIZE
 #if !SVT_AV1_CHECK_VERSION(1, 5, 0)
     /* DEPRECATED: to be removed in 1.5.0. */
     uint32_t vbv_bufsize;
 #endif
-
+#endif
     /* Maxium QP value allowed for rate control use, only applicable when rate
      * control mode is set to 1. It has to be greater or equal to minQpAllowed.
      *
@@ -458,7 +466,7 @@ typedef struct EbSvtAv1EncConfiguration {
      *
      * Default is 4. */
     uint32_t min_qp_allowed;
-
+#if !FIX_VBR_BIAS_PCT
     // DATARATE CONTROL OPTIONS
 #if !SVT_AV1_CHECK_VERSION(2, 0, 0)
     /* DEPRECATED: to be removed in 2.0.0. */
@@ -475,7 +483,7 @@ typedef struct EbSvtAv1EncConfiguration {
      */
     uint32_t vbr_bias_pct;
 #endif
-
+#endif
     /**
      * @brief Variable Bit Rate Minimum Section Percentage
      *
@@ -617,8 +625,11 @@ typedef struct EbSvtAv1EncConfiguration {
      *
      * Default is true.
      */
+#if FIX_BOOL
+    bool enable_dlf_flag;
+#else
     Bool enable_dlf_flag;
-
+#endif
     /* Film grain denoising the input picture
     * Flag to enable the denoising
     *
@@ -663,8 +674,11 @@ typedef struct EbSvtAv1EncConfiguration {
      *
      * Default is false.
      */
+#if FIX_BOOL
+    bool restricted_motion_vector;
+#else
     Bool restricted_motion_vector;
-
+#endif
     /* Log 2 Tile Rows and columns . 0 means no tiling,1 means that we split the dimension
         * into 2
         * Default is 0. */
@@ -711,9 +725,15 @@ typedef struct EbSvtAv1EncConfiguration {
      *
      * Default is true.
      */
+#if FIX_BOOL
+    bool enable_tf;
+
+    bool enable_overlays;
+#else
     Bool enable_tf;
 
     Bool enable_overlays;
+#endif
     /**
      * @brief Tune for a particular metric; 0: VQ, 1: PSNR, 2: SSIM.
      *
@@ -728,7 +748,7 @@ typedef struct EbSvtAv1EncConfiguration {
     uint8_t superres_qthres;
     uint8_t superres_kf_qthres;
     uint8_t superres_auto_search_type;
-
+#if !FIX_PRED_STRUCT
 #if !SVT_AV1_CHECK_VERSION(1, 5, 0)
     /* DEPRECATED: to be removed in 1.5.0. */
     PredictionStructureConfigEntry pred_struct[1 << (MAX_HIERARCHICAL_LEVEL - 1)];
@@ -738,6 +758,7 @@ typedef struct EbSvtAv1EncConfiguration {
 
     /* DEPRECATED: to be removed in 1.5.0. */
     int32_t manual_pred_struct_entry_num;
+#endif
 #endif
     /* Decoder-speed-targeted encoder optimization level (produce bitstreams that can be decoded faster).
     * 0: No decoder-targeted speed optimization
@@ -782,6 +803,7 @@ typedef struct EbSvtAv1EncConfiguration {
     // Threads management
 
 #if CLN_LP_LVLS
+#if !FIX_SVT_AV1_CHECK_VERSION
 #if !SVT_AV1_CHECK_VERSION(3, 0, 0)
     /* logical_processors refers to how much parallelization the encoder will perform
      * by setting the number of threads and pictures that can be handled simultaneously. If
@@ -791,7 +813,7 @@ typedef struct EbSvtAv1EncConfiguration {
      */
     uint32_t logical_processors;
 #endif
-
+#endif
     /* The level of parallelism refers to how much parallelization the encoder will perform
      * by setting the number of threads and pictures that can be handled simultaneously. If
      * the value is 0, a deafult level will be chosen based on the number of cores on the
@@ -846,6 +868,22 @@ typedef struct EbSvtAv1EncConfiguration {
      *
      * Default is false.
      */
+#if FIX_BOOL
+    bool recon_enabled;
+    // 1.0.0: Any additional fields shall go after here
+
+    /**
+     * @brief Signal that force-key-frames is enabled.
+     *
+     */
+    bool force_key_frames;
+
+    /**
+     * @brief Signal to the library to treat intra_period_length as seconds and
+     * multiply by fps_num/fps_den.
+     */
+    uint8_t multiply_keyint;
+#else
     Bool recon_enabled;
     // 1.0.0: Any additional fields shall go after here
 
@@ -860,7 +898,7 @@ typedef struct EbSvtAv1EncConfiguration {
      * multiply by fps_num/fps_den.
      */
     Bool multiply_keyint;
-
+#endif
     // reference scaling parameters
     /**
      * @brief Reference scaling mode
@@ -884,7 +922,11 @@ typedef struct EbSvtAv1EncConfiguration {
      *
      * Default is false.
      */
+#if FIX_BOOL
+    bool enable_qm;
+#else
     Bool enable_qm;
+#endif
     /**
      * @brief Min quant matrix flatness. Applicable when enable_qm is true.
      * Min value is 0.
@@ -911,8 +953,11 @@ typedef struct EbSvtAv1EncConfiguration {
      * 1: on
      * Default is 0.
      */
+#if FIX_BOOL
+    bool gop_constraint_rc;
+#else
     Bool gop_constraint_rc;
-
+#endif
     /**
      * @brief scale factors for lambda value for different frame update types
      * factor >> 7 (/ 128) is the actual value in float
@@ -924,8 +969,11 @@ typedef struct EbSvtAv1EncConfiguration {
     * 0 = disable Dynamic GoP
     * 1 = enable Dynamic GoP
     *  Default is 1. */
+#if FIX_BOOL
+    bool enable_dg;
+#else
     Bool enable_dg;
-
+#endif
     /**
      * @brief startup_mg_size
      *
@@ -966,8 +1014,11 @@ typedef struct EbSvtAv1EncConfiguration {
     * 0 = disable ROI
     * 1 = enable ROI
     *  Default is 0. */
+#if FIX_BOOL
+    bool enable_roi_map;
+#else
     Bool enable_roi_map;
-
+#endif
     /* Stores the optional film grain synthesis info */
     AomFilmGrain *fgs_table;
 
@@ -978,8 +1029,11 @@ typedef struct EbSvtAv1EncConfiguration {
      * false = disable variance boost
      * true = enable variance boost
      * Default is false. */
+#if FIX_BOOL
+    bool enable_variance_boost;
+#else
     Bool enable_variance_boost;
-
+#endif
     /* @brief Selects the curve strength to boost low variance regions according to a fast-growing formula
      * Default is 2 */
     uint8_t variance_boost_strength;
@@ -997,19 +1051,31 @@ typedef struct EbSvtAv1EncConfiguration {
      *
      * Default is false.
      */
+#if FIX_BOOL
+    bool lossless;
+#else
     Bool lossless;
+#endif
 #if FTR_STILL_PICTURE
     /* @brief Signal to the library to enable still-picture coding
      *
      * Default is false.
      */
+#if FIX_BOOL
+    bool avif;
+#else
     Bool avif;
+#endif
 #endif
     /*Add 128 Byte Padding to Struct to avoid changing the size of the public configuration struct*/
 #if CLN_LP_LVLS
 #if FTR_STILL_PICTURE
 #if FTR_STARTUP_QP
+#if FIX_PUBLIC_HEADER
+    uint8_t padding[128];
+#else
     uint8_t padding[128 - 3 * sizeof(Bool) - 2 * sizeof(uint8_t) - sizeof(uint32_t) - sizeof(int8_t)];
+#endif
 #else
     uint8_t padding[128 - 3 * sizeof(Bool) - 2 * sizeof(uint8_t) - sizeof(uint32_t)];
 #endif
@@ -1054,7 +1120,11 @@ EB_API void svt_av1_print_version(void);
      * @ *config_ptr     Pointer passed back to the client during callbacks, it will be
      *                  loaded with default params from the library. */
 EB_API EbErrorType svt_av1_enc_init_handle(
+#if FIX_P_APP_DATA
+    EbComponentType **p_handle,
+#else
     EbComponentType **p_handle, void *p_app_data,
+#endif
     EbSvtAv1EncConfiguration *config_ptr); // config_ptr will be loaded with default params from the library
 
 /* STEP 2: Set all configuration parameters.

.gitlab/workflows/linux/.gitlab-ci.yml
@@ -433,6 +433,7 @@ Linux libavif (Static):
     - git clone $PWD svtav1-src
     - git clone https://github.com/AOMediaCodec/libavif.git libavif-src
     - git clone https://code.videolan.org/videolan/dav1d.git dav1d-src
+    - git -C libavif-src apply $PWD/.gitlab/workflows/linux/libavif_api_job_fix.patch
     - !reference [.ffmpeg-before-script-export]
     - apt update && apt install -yyy libpng-dev libjpeg-dev
   script:
@@ -467,6 +468,7 @@ Linux FFmpeg (n7.0.2, Static):
     - !reference [.ffmpeg-before-script-git-setup]
     - !reference [.ffmpeg-before-script-clone]
     - git -C ffmpeg-src checkout n7.0.2
+    - git -C ffmpeg-src apply $PWD/.gitlab/workflows/linux/ffmpeg_n7_fix.patch
     - !reference [.ffmpeg-before-script-export]
   script:
     - !reference [.ffmpeg-svtav1-script]
@@ -510,6 +512,7 @@ Linux FFmpeg (n5.0, Static):
     - !reference [.ffmpeg-before-script-clone]
     - git -C ffmpeg-src checkout n5.0
     - git -C ffmpeg-src am -3 "$PWD/ffmpeg_plugin/n5.0/"*.patch
+    - git -C ffmpeg-src apply $PWD/.gitlab/workflows/linux/ffmpeg_n5_fix.patch
     - !reference [.ffmpeg-before-script-export]
   script:
     - !reference [.ffmpeg-svtav1-script]

.gitlab/workflows/linux/ffmpeg_n5_fix.patch
@@ -1,0 +1,13 @@
+diff --git a/libavcodec/libsvtav1.c b/libavcodec/libsvtav1.c
+index 120ead71b7..df6978c97f 100644
+--- a/libavcodec/libsvtav1.c
++++ b/libavcodec/libsvtav1.c
+@@ -382,7 +382,7 @@ static av_cold int eb_enc_init(AVCodecContext *avctx)
+ 
+     svt_enc->eos_flag = EOS_NOT_REACHED;
+ 
+-    svt_ret = svt_av1_enc_init_handle(&svt_enc->svt_handle, svt_enc, &svt_enc->enc_params);
++    svt_ret = svt_av1_enc_init_handle(&svt_enc->svt_handle, &svt_enc->enc_params);
+     if (svt_ret != EB_ErrorNone) {
+         return svt_print_error(avctx, svt_ret, "Error initializing encoder handle");
+     }

.gitlab/workflows/linux/ffmpeg_n7_fix.patch
@@ -1,0 +1,13 @@
+diff --git a/libavcodec/libsvtav1.c b/libavcodec/libsvtav1.c
+index 8fa42d590b..e99c656c5d 100644
+--- a/libavcodec/libsvtav1.c
++++ b/libavcodec/libsvtav1.c
+@@ -430,7 +430,7 @@ static av_cold int eb_enc_init(AVCodecContext *avctx)
+ 
+     svt_enc->eos_flag = EOS_NOT_REACHED;
+ 
+-    svt_ret = svt_av1_enc_init_handle(&svt_enc->svt_handle, svt_enc, &svt_enc->enc_params);
++    svt_ret = svt_av1_enc_init_handle(&svt_enc->svt_handle, &svt_enc->enc_params);
+     if (svt_ret != EB_ErrorNone) {
+         return svt_print_error(avctx, svt_ret, "Error initializing encoder handle");
+     }

.gitlab/workflows/linux/libavif_api_job_fix.patch
@@ -1,0 +1,31 @@
+diff --git a/src/codec_svt.c b/src/codec_svt.c
+index 193f244a..6ae4bc66 100644
+--- a/src/codec_svt.c
++++ b/src/codec_svt.c
+@@ -114,7 +114,7 @@ static avifResult svtCodecEncodeImage(avifCodec * codec,
+         // See https://gitlab.com/AOMediaCodec/SVT-AV1/-/issues/1697.
+         memset(svt_config, 0, sizeof(EbSvtAv1EncConfiguration));
+ 
+-        res = svt_av1_enc_init_handle(&codec->internal->svt_encoder, NULL, svt_config);
++        res = svt_av1_enc_init_handle(&codec->internal->svt_encoder, svt_config);
+         if (res != EB_ErrorNone) {
+             goto cleanup;
+         }
+@@ -126,13 +126,13 @@ static avifResult svtCodecEncodeImage(avifCodec * codec,
+ #endif
+ 
+         // Follow comment in svt header: set if input is HDR10 BT2020 using SMPTE ST2084 (PQ).
+-        svt_config->high_dynamic_range_input = (image->depth == 10 && image->colorPrimaries == AVIF_COLOR_PRIMARIES_BT2020 &&
+-                                                image->transferCharacteristics == AVIF_TRANSFER_CHARACTERISTICS_SMPTE2084 &&
+-                                                image->matrixCoefficients == AVIF_MATRIX_COEFFICIENTS_BT2020_NCL);
++        // svt_config->high_dynamic_range_input = (image->depth == 10 && image->colorPrimaries == AVIF_COLOR_PRIMARIES_BT2020 &&
++                                                // image->transferCharacteristics == AVIF_TRANSFER_CHARACTERISTICS_SMPTE2084 &&
++                                                // image->matrixCoefficients == AVIF_MATRIX_COEFFICIENTS_BT2020_NCL);
+ 
+         svt_config->source_width = image->width;
+         svt_config->source_height = image->height;
+-        svt_config->logical_processors = encoder->maxThreads;
++        svt_config->level_of_parallelism = encoder->maxThreads;
+         svt_config->enable_adaptive_quantization = 2;
+         // disable 2-pass
+ #if SVT_AV1_CHECK_VERSION(0, 9, 0)

.gitlab/workflows/macos/.gitlab-ci.yml
@@ -183,6 +183,7 @@ macOS FFmpeg (Static):
   before_script:
     - !reference [.ffmpeg-before-script-clone]
     - !reference [.ffmpeg-before-script-export]
+    - git -C ffmpeg-src apply $PWD/.gitlab/workflows/linux/ffmpeg_n7_fix.patch
   script:
     - !reference [.ffmpeg-svtav1-script]
     - !reference [.ffmpeg-aom-script]

.gitlab/workflows/windows/.gitlab-ci.yml
@@ -111,6 +111,7 @@ Win64 FFmpeg (Static):
     PREFIX_DIR: C:/msys64/mingw64
   before_script:
     - !reference [.ffmpeg-before-script-clone-windows]
+    - git -C ffmpeg-src apply $PWD/.gitlab/workflows/linux/ffmpeg_n7_fix.patch
   script:
     # SVT-AV1
     - |

Source/API/EbDebugMacros.h
@@ -96,10 +96,31 @@ extern "C" {
 #define FIX_FAST_PRESET             1 // Tuning for high presets
 #define TUNE_FD0_FEATS              1 // Unify CDEF and DLF levels of fd2/fd0 in M10 and M9
 #define OPT_CDEF_FD1                1 // Opt CDEF for fd1
+#define FIX_PUBLIC_HEADER           1
+#if FIX_PUBLIC_HEADER
+// EbSvtAv1Enc.h
+#define FIX_BOOL                           1 // Replace Bool with the standard bool
+#define FIX_HIGH_DYNAMIC_RANGE_INPUT       1 // Remove high_dynamic_range_input as hdr is not supported
+#define FIX_COLOR_DESCRIPTION_PRESENT_FLAG 1 // Remove color_description_present_flag as adding color description information is not supported
+#define FIX_RATE_CONTROL_MODE              1 // Use SvtAv1RcMode instead of uint32_t
+#define FIX_VBV_BUFSIZE                    1 // Remove vbv_bufsize as not used
+#define FIX_VBR_BIAS_PCT                   1 // Remove vbr_bias_pct as not used
+#define FIX_PRED_STRUCT                    1 // Remove the unused pred_struct signals
+// EbSvtAv1.h
+#define FIX_EB_SVT_IO_FORMAT               1 // Remove the unused EbSvtIOFormat fields
+#define FIX_P_APP_DATA                     1 // Remove p_application_private as not used
+#define FTR_SIGNAL_LAYER                   1 // Output the temporal layer index of the encoded picture
+#define FTR_SIGNAL_AVERAGE_QP              1 // Output the mean QP across all blocks for the encoded picture
+#endif
+#define FIX_SVT_AV1_CHECK_VERSION          1 // Remove the SVT_AV1_CHECK_VERSION instances
+#define FIX_NO_ENCDEC                      1 // Remove REMOVE_LP1_LPN_DIFF as not used
+#define FIX_REMOVE_LP1_LPN_DIFF            1 // remove NO_ENCDEC as not used
 //FOR DEBUGGING - Do not remove
 #define OPT_LD_LATENCY2         1 // Latency optimization for low delay - to keep the Macro for backwards testing until 3.0
 #define LOG_ENC_DONE            0 // log encoder job one
+#if !FIX_NO_ENCDEC
 #define NO_ENCDEC               0 // bypass encDec to test cmpliance of MD. complained achieved when skip_flag is OFF. Port sample code from VCI-SW_AV1_Candidate1 branch
+#endif
 #define DEBUG_TPL               0 // Prints to debug TPL
 #define DETAILED_FRAME_OUTPUT   0 // Prints detailed frame output from the library for debugging
 #define TUNE_CHROMA_SSIM        0 // Allows for Chroma and SSIM BDR-based Tuning
@@ -110,6 +131,13 @@ extern "C" {
 #define LAD_MG_PRINT            0 // Report LAD
 #define RC_NO_R2R               0 // This is a debugging flag for RC and makes encoder to run with no R2R in RC mode
                                   // Note that the speed might impacted significantly
+#if FIX_REMOVE_LP1_LPN_DIFF
+#if !RC_NO_R2R
+#define FTR_KF_ON_FLY_SAMPLE      0 // Sample code to signal KF
+#define FTR_RES_ON_FLY_SAMPLE     0 // Sample functions to change the resolution on the fly
+#define FTR_RATE_ON_FLY_SAMPLE     0 // Sample functions to change bit rate
+#endif
+#else
 #if RC_NO_R2R
 #define REMOVE_LP1_LPN_DIFF     1 // Disallow single-thread/multi-thread differences
 #else
@@ -117,6 +145,7 @@ extern "C" {
 #define FTR_KF_ON_FLY_SAMPLE      0 // Sample code to signal KF
 #define FTR_RES_ON_FLY_SAMPLE     0 // Sample functions to change the resolution on the fly
 #define FTR_RATE_ON_FLY_SAMPLE    0 // Sample functions to change bit rate
+#endif
 #endif
 // Super-resolution debugging code
 #define DEBUG_SCALING           0

Likhach42/SVT-AV1-NO-AVX512

commit 9b0cff2f201201ef0b99c422d8eab746933ad76f

API: bump ABI version due to previous backwards-incompatible changes
Building with various API clients has been broken since
988e930c1083ce518ead1d364e3a486e9209bf73 , as there have been no
changes regarding any defines that make it possible to check if
one is building against an older SVT-AV1 or a newer one.

Source/API/EbSvtAv1Enc.h
@@ -28,7 +28,7 @@ extern "C" {
  * has been modified, and reset anytime the major API version has
  * been changed. Used to keep track if a field has been added or not.
  */
-#define SVT_AV1_ENC_ABI_VERSION 0
+#define SVT_AV1_ENC_ABI_VERSION 1
 
 //***HME***
 